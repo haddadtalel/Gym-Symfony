959785313d8525e7584cbc868869ff7223bc497a
Talel HADDAD <Talel.HADDAD@tek-up.de>
2024-12-06 19:20:52 +0100

    befor bech tetnek

--- src/Repository/PackRepository.php
+++ src/Repository/PackRepository.php
@@ -16,28 +16,22 @@ class PackRepository extends ServiceEntityRepository
         parent::__construct($registry, Pack::class);
     }
 
-    //    /**
-    //     * @return Pack[] Returns an array of Pack objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function findMonthlyPacks()
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.duration = :duration')
+            ->setParameter('duration', 'Monthly') 
+            ->getQuery()
+            ->getResult();
+    }
 
-    //    public function findOneBySomeField($value): ?Pack
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+ 
+    public function findYearlyPacks()
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.duration = :duration')
+            ->setParameter('duration', 'Yearly')  // Filter by 'yearly'
+            ->getQuery()
+            ->getResult();
+    }
 }
